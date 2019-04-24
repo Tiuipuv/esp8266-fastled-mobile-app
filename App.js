5a951f849202d4505de6d9ed7142a411bd17fc09
Jordan Jensen <tiuipuv@gmail.com>
2019-04-24 16:43:31 -0500

    slight changes to test

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
+    'Double tap R on your keyboard to reloads!,\n' +
     'Shake or press menu button for dev menu',
 });
 
